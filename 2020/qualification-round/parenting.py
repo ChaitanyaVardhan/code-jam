570c389c8a985072cf64b052c2cdc33be9c0916c
ChaitanyaVardhan <chaitanyavardhan1@gmail.com>
2020-04-04 13:04:05 +0530

    parenting brute force: v1

---
+++ 2020/qualification-round/parenting.py
@@ -0,0 +1,54 @@
+def solve(I):
+    sorted_I = sorted(I, key = lambda i: i[1])
+    
+    sorted_sched = []
+    sorted_sched.append('J')
+    J_prev_finish_time = sorted_I[0][1]
+    for i in range(1, len(sorted_I)):
+        if sorted_I[i][0] >= J_prev_finish_time:
+            sorted_sched.append('J')
+            J_prev_finish_time = sorted_I[i][1]
+        else:
+            sorted_sched.append('C')
+
+
+    C_prev_finish_time = 0
+    for i in range(len(sorted_sched)):
+        if sorted_sched[i]  == "C":
+            C_start_time = sorted_I[i][0]
+            if C_start_time >= C_prev_finish_time:
+                C_prev_finish_time = sorted_I[i][1]
+            else:
+                sorted_sched[i] = None
+        else:
+            continue
+
+    unsorted_sched = [0] * len(I)
+    i = 0
+    while i <len(I):
+        j = 0
+        while j < len(sorted_I):
+            if I[i] != sorted_I[j]:
+                j +=1
+            else:
+                unsorted_sched[i] = sorted_sched[j]
+                j = len(sorted_I)
+        i +=1
+
+
+    if None in unsorted_sched:
+        return "IMPOSSIBLE"
+    else:
+        return ''.join(unsorted_sched)
+    
+    
+if __name__ == "__main__":
+    T = int(input())
+    for i in range(T):
+        N = int(input())
+        I = []
+        for _ in range(N):
+            m, n = [int(s) for s in input().split(" ")]
+            I.append((m,n))
+        ans = solve(I)
+        print("Case #{}: {}".format(str(i+1), ans))
