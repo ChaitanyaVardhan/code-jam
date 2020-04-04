32721edf14e0a4769706cd69f53b725fe356370c
ChaitanyaVardhan <chaitanyavardhan1@gmail.com>
2020-04-04 15:07:31 +0530

    parenting: v3

--- 2020/qualification-round/parenting.py
+++ 2020/qualification-round/parenting.py
@@ -1,35 +1,43 @@
 def solve(I):
-    unsorted_sched = []
-    unsorted_sched.append('J')
-    J_prev_finish_time = I[0][1]
-    J_prev_start_time = I[0][0]
-    for i in range(1, len(I)):
-        if I[i][0] >= J_prev_finish_time:
-            unsorted_sched.append('J')
-            J_prev_finish_time = I[i][1]
-        elif I[i][1] < J_prev_start_time:
-            unsorted_sched.append('J')
-            J_prev_start_time = I[i][0]
-        else:
-            unsorted_sched.append('C')
+    unsorted_sched_1 = solve_for_first(I, 'J', 'C')
+    unsorted_sched_2 = solve_for_first(I, 'C', 'J')
+    if "X" in unsorted_sched_1 and "X" in unsorted_sched_2:
+        return "IMPOSSIBLE"
+    elif "X" in unsorted_sched_1:
+        return ''.join(unsorted_sched_2)
+    else:
+        return ''.join(unsorted_sched_1)
+    
 
-    C_prev_finish_time = -1
-    C_prev_start_time = -2
+def solve_for_first(I, parent, other):
+    unsorted_sched = []
+    unsorted_sched.append(parent)
+    p1_prev_finish_time = I[0][1]
+    p1_prev_start_time = I[0][0]
+    for i in range(1, len(I)):
+        if I[i][0] >= p1_prev_finish_time:
+            unsorted_sched.append(parent)
+            p1_prev_finish_time = I[i][1]
+        elif I[i][1] < p1_prev_start_time:
+            unsorted_sched.append(parent)
+            p1_prev_start_time = I[i][0]
+        else:
+            unsorted_sched.append(other)
+
+    p2_prev_finish_time = -1
+    p2_prev_start_time = -2
     for i in range(len(unsorted_sched)):
-        if unsorted_sched[i]  == "C":
-            if I[i][0] >= C_prev_finish_time:
-                C_prev_finish_time = I[i][1]
-            elif I[i][1] < C_prev_start_time:
-                C_prev_start_time = I[i][0]
+        if unsorted_sched[i]  == other:
+            if I[i][0] >= p2_prev_finish_time:
+                p2_prev_finish_time = I[i][1]
+            elif I[i][1] < p2_prev_start_time:
+                p2_prev_start_time = I[i][0]
             else:
                 unsorted_sched[i] = 'X'
         else:
             continue
 
-    if "X" in unsorted_sched:
-        return "IMPOSSIBLE"
-    else:
-        return ''.join(unsorted_sched)
+    return unsorted_sched
     
     
 if __name__ == "__main__":
