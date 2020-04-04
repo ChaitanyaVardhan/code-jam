ef33f7e4f474300298c92c334a79bf2d186a0ae5
ChaitanyaVardhan <chaitanyavardhan1@gmail.com>
2020-04-04 14:48:53 +0530

    parenting: v2

--- 2020/qualification-round/parenting.py
+++ 2020/qualification-round/parenting.py
@@ -1,37 +1,30 @@
 def solve(I):
-    sorted_I = sorted(I, key = lambda i: i[1])
-    
-    sorted_sched = []
-    sorted_sched.append('J')
-    J_prev_finish_time = sorted_I[0][1]
-    for i in range(1, len(sorted_I)):
-        if sorted_I[i][0] >= J_prev_finish_time:
-            sorted_sched.append('J')
-            J_prev_finish_time = sorted_I[i][1]
+    unsorted_sched = []
+    unsorted_sched.append('J')
+    J_prev_finish_time = I[0][1]
+    J_prev_start_time = I[0][0]
+    for i in range(1, len(I)):
+        if I[i][0] >= J_prev_finish_time:
+            unsorted_sched.append('J')
+            J_prev_finish_time = I[i][1]
+        elif I[i][1] < J_prev_start_time:
+            unsorted_sched.append('J')
+            J_prev_start_time = I[i][0]
         else:
-            sorted_sched.append('C')
+            unsorted_sched.append('C')
 
     C_prev_finish_time = -1
-    for i in range(len(sorted_sched)):
-        if sorted_sched[i]  == "C":
-            if sorted_I[i][0] >= C_prev_finish_time:
-                C_prev_finish_time = sorted_I[i][1]
+    C_prev_start_time = -2
+    for i in range(len(unsorted_sched)):
+        if unsorted_sched[i]  == "C":
+            if I[i][0] >= C_prev_finish_time:
+                C_prev_finish_time = I[i][1]
+            elif I[i][1] < C_prev_start_time:
+                C_prev_start_time = I[i][0]
             else:
-                sorted_sched[i] = 'X'
+                unsorted_sched[i] = 'X'
         else:
             continue
-
-    unsorted_sched = [0] * len(I)
-    i = 0
-    while i < len(I):
-        j = 0
-        while j < len(sorted_I):
-            if I[i] != sorted_I[j]:
-                j +=1
-            else:
-                unsorted_sched[i] = sorted_sched[j]
-                j = len(sorted_I)
-        i +=1
 
     if "X" in unsorted_sched:
         return "IMPOSSIBLE"
